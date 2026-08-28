5e5a97f41f4a5ec55b53cd848406b2b1f5767b74
lyh <>
2026-08-28 16:29:26 +0800

    Fix: Correct SVG icons and restore Tabs green frosted glass effect to dial base

---
+++ generate_drink_svgs_fix2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DIR="entry/src/main/resources/base/media"
+
+cat << 'SVGEOF' > $DIR/ic_drink_herbal.svg
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333333">
+  <path d="M21 3H7c-1.1 0-2 .9-2 2v8c0 3.31 2.69 6 6 6h6c3.31 0 6-2.69 6-6V5c0-1.1-.9-2-2-2zm0 10c0 2.21-1.79 4-4 4H11c-2.21 0-4-1.79-4-4V5h14v8zM4 19h16v2H4z"/>
+</svg>
+SVGEOF
+
+cat << 'SVGEOF' > $DIR/ic_drink_bottle.svg
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333333">
+  <path d="M14 2H10v4.29c0 .35-.11.7-.31.98L4.1 14.88c-.69.98-.69 2.28 0 3.25.66.94 1.76 1.48 2.91 1.48h10c1.14 0 2.25-.54 2.91-1.48.69-.98.69-2.28 0-3.25l-5.59-7.61c-.2-.28-.31-.63-.31-.98V2zM7 17.5l4-5.46V4h2v8.04l4 5.46H7z"/>
+</svg>
+SVGEOF
+
+cat << 'SVGEOF' > $DIR/ic_action_drink2.svg
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333333">
+  <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/>
+</svg>
+SVGEOF
